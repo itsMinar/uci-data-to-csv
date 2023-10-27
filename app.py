99c4b4ae5bd135da83d452ec3aa072736f1aeddc
Md. Minar Munshi <minarmunshi46@gmail.com>
2023-10-28 01:34:32 +0600

    Identify non-numeric columns and fill missing values with rounded mean value only for the numeric columns

--- app.py
+++ app.py
@@ -8,14 +8,26 @@ raw_data_set = fetch_ucirepo(id=45)
 # Create a DataFrame from the original dataset
 df = pd.DataFrame(raw_data_set.data.original)
 
+# Find columns that are not of type int or float
+non_numeric_columns = [
+    col for col in df.columns if not pd.api.types.is_numeric_dtype(df[col])]
+
+
 # Check for NaN values in each column
 nan_columns = df.columns[df.isna().any()].tolist()
 
-# Calculate the mean value and round them to the nearest integer then Replace NaN values with the calculated the mean value of each column
+# For each column with NaN (missing) values in the DataFrame
 for column in nan_columns:
-    mean_value = df[column].mean()
-    df[column].fillna(round(mean_value), inplace=True)
-    print(column, "=> column's NaN value is filled with rounded mean value")
+    # Check if the column is not in the list of non-numeric columns
+    if column not in non_numeric_columns:
+        # Calculate the mean value of the column and make it round figure
+        rounded_mean_value = round(df[column].mean())
+
+        # Fill the NaN values in the column with the rounded mean value
+        df[column].fillna(rounded_mean_value, inplace=True)
+
+        # Print a message indicating that NaN values in the column have been filled with the rounded mean value
+        print(column, "=> column's NaN value is filled with rounded mean value")
 
 
 # Save the DataFrame to a CSV file named 'data.csv'
