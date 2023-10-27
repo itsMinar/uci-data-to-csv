6a50ac5c5c10d3f0d3820ec1770af198be78f8dc
Md. Minar Munshi <minarmunshi46@gmail.com>
2023-10-28 00:59:58 +0600

    Do Some Minor Refactoring

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Import necessary libraries
-from ucimlrepo import fetch_ucirepo
 import pandas as pd
+from ucimlrepo import fetch_ucirepo
 
 # Fetch a dataset with the ID from the UCI ML Repository, here I used 45(Heart Disease)
 raw_data_set = fetch_ucirepo(id=45)
@@ -15,10 +15,11 @@ nan_columns = df.columns[df.isna().any()].tolist()
 for column in nan_columns:
     mean_value = df[column].mean()
     df[column].fillna(round(mean_value), inplace=True)
+    print(column, "=> column's NaN value is filled with rounded mean value")
 
 
 # Save the DataFrame to a CSV file named 'data.csv'
 df.to_csv('data.csv', index=False)
 
 # Print a success message
-print('Successfully created the CSV file. Thanks!')
+print('\nSuccessfully created the CSV file. Thanks!')
